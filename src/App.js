c9a044262759e18c85c2acbb7f6ea7d271889f79
Fill <gcodepost@gmail.com>
2020-01-17 08:07:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+    return ( 
+    <div className = "app-wrapper"  >
+        <header className = 'header'>
+            <img src="" alt=""/>
+        </header>
+        <nav className ='nav'>
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Settings
+            </div>
+        </nav>
+        <div className = 'content'>
+            <div>
+                <img src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt=""/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
     </div>
-  );
+    );
 }
+
 
 export default App;
