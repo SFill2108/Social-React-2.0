6eedc56c33ea9cf45114924f7beb8340556673ec
Fill <gcodepost@gmail.com>
2020-01-27 02:08:39 +0300

    dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Денис
+                </div>
+                <div className={s.dialog}>
+                    Маша
+                </div>
+                <div className={s.dialog}>
+                    Дима
+                </div>
+                <div className={s.dialog}>
+                    Саня
+                </div>
+                <div className={s.dialog}>
+                    Гизмо
+                </div>
+                <div className={s.dialog}>
+                    Соболь
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Helloy</div>
+                <div className={s.message}>LOL</div>
+            </div>
         </div>
     );
 }
