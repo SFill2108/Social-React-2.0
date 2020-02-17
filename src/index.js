04bb5e963195b4e70eb5cd60fe417ab82c2930c3
Fill <gcodepost@gmail.com>
2020-02-18 02:04:49 +0300

    delete router.js and add observer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './render';
-import state from './Redux/state';
+import state, {subscribe} from './Redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from './Redux/state';
+import {BrowserRouter} from 'react-router-dom';
 
 
+let rerenderEntireTree = (state) => {
+    ReactDOM.render( 
+    < BrowserRouter >
+        <App state = { state } addPost = { addPost } updateNewPostText={updateNewPostText}/>  
+    </BrowserRouter > , document.getElementById('root'));
+    }
 rerenderEntireTree(state);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(state);
+
+subscribe(rerenderEntireTree); // callback в state.js во избежание зацикливания
